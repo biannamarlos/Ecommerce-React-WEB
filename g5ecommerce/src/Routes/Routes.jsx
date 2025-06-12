e5c4c09e782d2a6a10493ea297cb388afb06eb16
Enzo Dias <lenzo7085@gmail.com>
2025-06-12 19:18:15 -0300

    colocando a caixa

--- g5ecommerce/src/Routes/Routes.jsx
+++ g5ecommerce/src/Routes/Routes.jsx
@@ -1,20 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { HomePage } from "../pages/homepage/home";
-import { FormPage } from "../pages/form/form";
-import { About } from "../pages/about/about";
-import { NotFoundPage } from "../pages/notFound/not-found";
-import { Products } from "../pages/products/products";
 import { Register } from "../pages/register/register";
 
 export function Rotas() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/formulario" element={<FormPage />} />
+      <Route path="/" element={<Register/>}/>
       <Route path="/cadastrar" element={<Register/>}/>
-      <Route path="/about" element={<About />} />
-      <Route path="/*" element={<NotFoundPage />} />
-      <Route path="/products" element={<Products />} />
     </Routes>
   );
 }
+//corrigir depois
--- g5ecommerce/src/pages/register/register.jsx
+++ g5ecommerce/src/pages/register/register.jsx
@@ -2,10 +2,12 @@ import styles from './register.module.css'
 
 export function Register(){
     return (
-        <>
             <div className={styles.registerEdit}>
-                <h1>teste</h1>
+                <div className={styles.caixaEdit}>
+                    <div className={styles.tituloEdit}>
+                        <h1>Cadastro</h1>
+                    </div>
+                </div>
             </div>
-        </>
     )
 }
