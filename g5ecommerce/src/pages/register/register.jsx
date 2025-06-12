3aaea49560868677c770a1e4f20921237cedd75a
Enzo Dias <lenzo7085@gmail.com>
2025-06-12 18:30:29 -0300

    atualizando base

---
+++ g5ecommerce/src/pages/register/register.jsx
@@ -0,0 +1,11 @@
+import styles from './register.module.css'
+
+export function Register(){
+    return (
+        <>
+            <div className={styles.registerEdit}>
+                <h1>teste</h1>
+            </div>
+        </>
+    )
+}
