4ef3b95652441cabc5f56c85a02e9a4137a77532
Mateus Karl Peixoto <mateuskpeixoto@gmail.com>
2025-06-13 12:51:18 -0300

    Layout fixes

--- g5ecommerce/src/Routes/Routes.jsx
+++ g5ecommerce/src/Routes/Routes.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-
+import { HomePage } from "../pages/home/home"
 import { Register } from "../pages/register/register";
 
 export function Rotas() {
   return (
     <Routes>
-      <Route path="/" element={<Register/>}/>
+      <Route path="/" element={<HomePage/>}/>
       <Route path="/cadastrar" element={<Register/>}/>
     </Routes>
   );
