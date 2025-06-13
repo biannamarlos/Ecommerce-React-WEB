f4cbe3d4ad69411b4105cc1ca339a68eabe207be
Adriana Franco <adri.franco2@yahoo.com.br>
2025-06-13 20:53:14 -0300

    primeiro commit login

--- g5ecommerce/src/Routes/Routes.jsx
+++ g5ecommerce/src/Routes/Routes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/home/home"
 import { Register } from "../pages/register/register";
+import { Login } from "../Login/login";
 
 export function Rotas() {
   return (
     <Routes>
       <Route path="/" element={<HomePage/>}/>
       <Route path="/cadastrar" element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
     </Routes>
   );
 }
