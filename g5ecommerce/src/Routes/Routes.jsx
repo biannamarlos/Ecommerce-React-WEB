503d25e0279d8ff0f3afae07fbc57094bc9cba44
Dandara <dandaralferreira08@gmail.com>
2025-06-13 21:29:51 -0300

    Organizando

--- g5ecommerce/src/Routes/Routes.jsx
+++ g5ecommerce/src/Routes/Routes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/home/home"
 import { Register } from "../pages/register/register";
+import Profile from "../pages/profile/profile";
 
 export function Rotas() {
   return (
     <Routes>
       <Route path="/" element={<HomePage/>}/>
       <Route path="/cadastrar" element={<Register/>}/>
+      <Route path="/perfil" element={<Profile/>}/>
     </Routes>
   );
 }
